59b0238218d28fe8a012f855c1e70549c0def08f
m_matsubara <m_matsubara@okwave.co.jp>
2015-01-22 19:12:25 +0900

    test user投入

---
+++ config/flextures.config.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+module Flextures
+  # test/fixtures/ のフィクスチャを読み出したい場合は吐き出しディレクトリの値を上書きする
+  Config.fixture_load_directory = "test/fixtures/"
+  Config.fixture_dump_directory = "test/fixtures/"
+  Config.init_all_tables = true
+  Config.use_transactional_fixtures = false
+  #Config.ignore_tables= (Config.ignore_tables.concat ["admin_users"])
+end
