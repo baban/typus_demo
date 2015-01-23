59b602e7793b3eb140d2a0550e94d99c697b848f
m_matsubara <m_matsubara@okwave.co.jp>
2015-01-23 10:27:45 +0900

    typus 基本設定

--- config/initializers/typus.rb
+++ config/initializers/typus.rb
@@ -2,7 +2,6 @@ Typus.setup do |config|
 
   # Application name.
   # config.admin_title = "Typus"
-
   # When mailer_sender is set, password recover is enabled. This email
   # address will be used in Admin::Mailer.
   #
